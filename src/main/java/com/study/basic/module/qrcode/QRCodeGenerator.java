16d8dfb3db23262f2717a6ab870bf27c8be6d409
dzp <846587131@qq.com>
2021-02-19 18:11:13 +0800

    qrcode支持中文

--- src/main/java/com/study/basic/module/qrcode/QRCodeGenerator.java
+++ src/main/java/com/study/basic/module/qrcode/QRCodeGenerator.java
@@ -1,6 +1,7 @@
 package com.study.basic.module.qrcode;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
@@ -9,6 +10,8 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author dzp 2021/2/19
@@ -17,15 +20,18 @@ public class QRCodeGenerator {
 
     public static void generateQRCodeImage(String text, int width, int height, String filePath)
             throws WriterException, IOException {
+
+        Map<EncodeHintType, String> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints);
         Path path = FileSystems.getDefault().getPath(filePath);
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
     }
 
     public static void main(String[] args) {
         try {
-            generateQRCodeImage("WDNMD", 350, 350, "D:\\Project\\workspace\\basic\\src\\main\\resources\\images\\QRTest.png");
+            generateQRCodeImage("傻逼，扫你嘛个头！", 350, 350, "D:\\Project\\workspace\\basic\\src\\main\\resources\\images\\QRTest.png");
         } catch (WriterException | IOException e) {
             e.printStackTrace();
         }
